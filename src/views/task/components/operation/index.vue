ac2407a0135673d86db1bfee62286becc32d4201
bianling <1343881168@qq.com>
2022-08-06 21:15:48 +0800

    完成搜索框的静态页面搭建

--- src/views/task/components/operation/index.vue
+++ src/views/task/components/operation/index.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>operation</div>
+  <div>
+    <SearchBar lastInpTitle="工单编号" rightInpTitle="工单状态" />
+    <div class="result">
+      <div class="btn-title">
+        <MyButton icon="el-icon-circle-plus-outline" :btnStyle="addBtnStyle"
+          >新建</MyButton
+        >
+        <!-- <FooterPage /> -->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SearchBar from "@/components/SerchBar";
+import MyButton from "@/components/MyButton";
+import FooterPage from "@/components/FooterPage";
 export default {
-  name: "",
+  name: "Operation",
   data() {
-    return {};
+    return {
+      addBtnStyle: {
+        background: "linear-gradient(135deg,#ff9743,#ff5e20)",
+      },
+    };
   },
 
   created() {},
@@ -14,7 +31,18 @@ export default {
   methods: {},
 
   computed: {},
+
+  components: {
+    SearchBar,
+    MyButton,
+    FooterPage,
+  },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.result {
+  padding: 20px 15px 19px 17px;
+  background-color: #fff;
+}
+</style>
