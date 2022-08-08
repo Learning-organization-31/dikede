290774c0805f3a63f8f605560937c3265d9398a7
xx0tt <307457823@qq.com>
2022-08-08 09:09:56 +0800

    拉取新代码

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import getters from './getters'
-import app from './modules/app'
-import settings from './modules/settings'
-import user from './modules/user'
-import task from './modules/task'
-import policy from './modules/policy'
-import createVuexPersisted from 'vuex-persistedstate'
-import vm from './modules/vm'
-import people from './modules/people'
-import region from './modules/region'
-import node from './modules/node'
-import partner from './modules/partner'
-import operation from './modules/operation'
+import Vue from "vue";
+import Vuex from "vuex";
+import getters from "./getters";
+import app from "./modules/app";
+import settings from "./modules/settings";
+import user from "./modules/user";
+import task from "./modules/task";
+import policy from "./modules/policy";
+import createVuexPersisted from "vuex-persistedstate";
+import vm from "./modules/vm";
+import people from "./modules/people";
+import region from "./modules/region";
+import node from "./modules/node";
+import partner from "./modules/partner";
+import operation from "./modules/operation";
 
 Vue.use(Vuex);
 
@@ -38,10 +38,10 @@ const store = new Vuex.Store({
           user: {
             userInfo: state.user.userInfo,
           },
-        }
+        };
       },
     }),
   ],
-})
+});
 
-export default store
+export default store;
