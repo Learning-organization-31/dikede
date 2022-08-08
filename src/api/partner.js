c7e367681a641469a7bde764816d5a5b338593df
bianling <1343881168@qq.com>
2022-08-08 11:49:42 +0800

    完成了合作商管理页面

--- src/api/partner.js
+++ src/api/partner.js
@@ -12,3 +12,53 @@ export const partnerSearch = (params) => {
     params,
   });
 };
+
+/**
+ * 初始化合作商密码
+ * @param {String} id
+ * @returns
+ */
+export const initPassword = (id) => {
+  return request({
+    url: "/api/user-service/partner/resetPwd/" + id,
+    method: "PUT",
+  });
+};
+
+/**
+ * 新增合作商
+ * @param {Object} data
+ * @returns
+ */
+export const addPartner = (data) => {
+  return request({
+    url: "/api/user-service/partner",
+    method: "POST",
+    data,
+  });
+};
+
+/**
+ * 修改合作商
+ * @param {Object} data
+ * @returns
+ */
+export const setPartner = (data) => {
+  return request({
+    url: "/api/user-service/partner/" + data.id,
+    method: "PUT",
+    data,
+  });
+};
+
+/**
+ * 删除合作商
+ * @param {String} id
+ * @returns
+ */
+export const clearPartner = (id) => {
+  return request({
+    url: "/api/user-service/partner/" + id,
+    method: "DELETE",
+  });
+};
