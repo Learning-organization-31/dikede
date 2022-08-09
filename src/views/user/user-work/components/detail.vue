ec65ae19558d6f9690d1582b9cfe0fecf7b9876c
webxf <j17852533489@163.com>
2022-08-09 15:43:01 +0800

    完成人员列表工作量列表

--- src/views/user/user-work/components/detail.vue
+++ src/views/user/user-work/components/detail.vue
@@ -1,69 +1,112 @@
 <template>
-  <div>
-    <el-dialog
-      title="收货地址"
-      :visible.sync="dialogTableVisible"
-      @close="close"
-      @open="open"
-    >
-      <el-table :data="gridData">
-        <el-table-column
-          property="date"
-          label="日期"
-          width="150"
-        ></el-table-column>
-        <el-table-column
-          property="name"
-          label="姓名"
-          width="200"
-        ></el-table-column>
-        <el-table-column property="address" label="地址"></el-table-column>
-      </el-table>
-    </el-dialog>
+  <!-- 搜索区域 -->
+  <div class="search">
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item :label="lastInpTitle + ':'">
+        <el-input
+          ref="lastInput"
+          v-model="taskCode"
+          placeholder="请输入"
+          clearable
+        >
+        </el-input>
+      </el-form-item>
+      <el-form-item :label="rightInpTitle + ':'" v-if="rightInpTitle">
+        <el-select v-model="status" placeholder="请选择" :clearable="true">
+          <el-option
+            :label="item.roleName"
+            :value="item.roleId"
+            v-for="item in typeList"
+            :key="item.roleId"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <MyButton
+          @click="searchBtnFn"
+          icon="el-icon-search"
+          :btnStyle="searchButStyle"
+        >
+          查询
+        </MyButton>
+      </el-form-item>
+    </el-form>
   </div>
+  <!-- 搜索区域 -->
 </template>
 
 <script>
+import MyButton from "@/components/MyButton";
 export default {
+  name: "SearchBar",
   data() {
     return {
-      gridData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-      ],
-      dialogTableVisible: false,
+      taskCode: "",
+      status: "",
+
+      //搜索区域按钮样式
+      searchButStyle: {
+        background: "#5f84ff",
+      },
     };
+  },
+
+  props: {
+    lastInpTitle: {
+      type: String,
+    },
+    rightInpTitle: {
+      type: String,
+    },
+    typeList: {
+      type: Array,
+    },
   },
 
   created() {},
 
   methods: {
-    close() {
-      this.$emit("close");
+    //搜索按钮获取数据
+    searchBtnFn() {
+      this.$emit("search", this.taskCode, this.status);
     },
-    open() {
-      this.$emit("open");
-    },
+  },
+
+  computed: {},
+
+  components: {
+    MyButton,
   },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.search {
+  display: flex;
+  align-items: center;
+  height: 64px;
+  background-color: #fff;
+  padding-left: 17px;
+  margin-bottom: 20px;
+  .el-form {
+    display: flex;
+    align-items: center;
+  }
+  .el-form-item {
+    margin-bottom: 0;
+  }
+}
+::v-deep .el-form-item__label {
+  font-weight: 400;
+}
+//输入框的样式
+::v-deep .demo-form-inline {
+  .el-icon-circle-close {
+    cursor: pointer;
+  }
+  input {
+    width: 206px;
+    height: 36px;
+  }
+}
+</style>
