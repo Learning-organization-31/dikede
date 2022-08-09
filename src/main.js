f41ecec184e02b936864a39593584abab0664a89
xx0tt <307457823@qq.com>
2022-08-09 16:26:12 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -6,30 +6,35 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import locale from "element-ui/lib/locale/lang/en"; // lang i18n
 
-import * as echarts from "echarts";
-
-
-
 import "@/styles/index.scss"; // global css
 
 import App from "./App";
 import store from "./store";
 import router from "./router";
 
-
 import * as directives from "@/directives";
+
+import "@/icons"; // icon
+import "@/permission"; // permission control
+
+import * as echarts from "echarts";
 
 if (process.env.NODE_ENV === "production") {
   const { mockXHR } = require("../mock");
   mockXHR();
 }
 
+// 如果想要中文版 element-ui，按如下方式声明
 Vue.use(ElementUI);
-Vue.prototype.$echarts = echarts;
 
 Vue.config.productionTip = false;
 
+Vue.prototype.$echarts = echarts;
 
+// 注册全局自定义事件
+for (let k in directives) {
+  Vue.directive(k, directives[k]);
+}
 
 new Vue({
   el: "#app",
