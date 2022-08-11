734db2d5191076d9ca15aea87342a6fd858ebda3
renbuairen <1664844124@qq.com>
2022-08-11 13:46:42 +0800

    修复了bug

--- src/views/report/index.vue
+++ src/views/report/index.vue
@@ -156,7 +156,12 @@ export default {
     ...mapActions('report', ['setPartnersList', 'setPartnerNameList']),
 
     async searchFn(val) {
-      if (val?.[0] !== this.searchVal?.[0]) this.page = 1
+      if (
+        val?.[0] !== this.searchVal?.[0] ||
+        val?.[1][0] !== this.searchVal?.[1][0] ||
+        val?.[1][1] !== this.searchVal?.[1][1]
+      )
+        this.page = 1
       if (!val) {
         val = [null, [dayjs().startOf('month').$d, dayjs().$d]]
       }
